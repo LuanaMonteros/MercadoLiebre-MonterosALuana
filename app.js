6b7ecc33db29dc44e27abee1bebc2477c0f372ad
LuanaMonteros <luamonteros08@gmail.com>
2023-11-15 18:53:28 -0300

    ahora si commiteando

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPathFold = path.resolve(__dirname, './public')
+
+app.use(express.static(publicPathFold));
+
+// app.listen(3000, () => {
+//     console.log('Servidor corriendo en http://localhost:3000/')
+// });    
+ 
+app.post('/', (req, res) => {
+    res.sendFile(path.resolve('views/home.html'))
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, ()=> console.log(`Servidor corriendo en http://localhost:{PORT}`));
