6b4ff85612d00b272b189de8b352a551156fb1d8
LuanaMonteros <luamonteros08@gmail.com>
2023-11-15 19:35:03 -0300

    solucion(?

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const publicPathFold = path.resolve(__dirname, './public')
 app.use(express.static(publicPathFold));
 
 // app.listen(3000, () => {
-//     console.log('Servidor corriendo en http://localhost:3000/')
-// });    
+    //     console.log('Servidor corriendo en http://localhost:3000/')
+    // });    
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, ()=> console.log(`Servidor corriendo en http://localhost:${PORT}`));
  
 app.post('/', (req, res) => {
     res.sendFile(path.resolve('views/home.html'))
@@ -21,6 +23,3 @@ app.get('/register', (req,res)=>{
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, ()=> console.log(`Servidor corriendo en http://localhost:${PORT}`));
