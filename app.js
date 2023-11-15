7103255c6e39f53005cedaced748cf7139db4104
LuanaMonteros <luamonteros08@gmail.com>
2023-11-15 19:51:01 -0300

    commiteando por enesima vez

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const publicPathFold = path.resolve(__dirname, './public')
 
+
 app.use(express.static(publicPathFold));
 
 // app.listen(3000, () => {
@@ -12,14 +13,17 @@ app.use(express.static(publicPathFold));
     const PORT = process.env.PORT || 3001;
     app.listen(PORT, ()=> console.log(`Servidor corriendo en http://localhost:${PORT}`));
  
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./views/home.html'))
+});
 app.post('/', (req, res) => {
-    res.sendFile(path.resolve('views/home.html'))
+    res.sendFile(path.resolve('./views/home.html'))
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(__dirname + './views/register.html');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(__dirname + './views/login.html');
 });
